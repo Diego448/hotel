53d2c545a07f3d5d9a2f05fe7b3c562b12be71ac
Diego Serrano Gómez <diegosg448@gmail.com>
2017-03-05 23:57:52 -0600

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(103, 96)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Verificar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(30, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Mes:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 56)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(28, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Día:"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"})
+        Me.ComboBox1.Location = New System.Drawing.Point(40, 19)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(138, 21)
+        Me.ComboBox1.TabIndex = 3
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"})
+        Me.ComboBox2.Location = New System.Drawing.Point(40, 53)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(138, 21)
+        Me.ComboBox2.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(185, 127)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Hotel"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,57 @@
+﻿Public Class Form1
+    Dim cantidadDias(26) As Integer
+    Dim rangoA(26) As Integer
+    Dim rangoB(26) As Integer
+    Dim diaAño As Integer
+    Dim mes As Integer
+    Dim grupo As Integer
+    Dim cantidadMiembros As Integer
+    Dim diaMes As Integer
+
+    Sub Inicializar()
+        rangoA(1) = 1
+        rangoB(1) = 2
+        cantidadDias(26) = 13
+        rangoA(26) = 351
+        rangoB(26) = 360
+        diaMes = ComboBox2.SelectedIndex + 1
+        For i = 1 To 25
+            cantidadDias(i) = i + 1
+            'Console.WriteLine(cantidadDias(i))
+        Next
+        For i = 2 To 25
+            rangoA(i) = rangoB(i - 1) + 1
+            rangoB(i) = rangoA(i) + (cantidadDias(i) - 1)
+            'Console.WriteLine("({0}, {1})", rangoA(i), rangoB(i))
+        Next
+        Console.WriteLine()
+        If ComboBox1.SelectedIndex >= 0 And ComboBox1.SelectedIndex < 12 Then
+            mes = CInt(ComboBox1.SelectedItem.ToString)
+            If mes = 1 Then
+                'Console.WriteLine(ComboBox1.SelectedItem.ToString)
+                diaAño = CInt(ComboBox2.SelectedItem.ToString)
+            Else
+                diaAño = ((mes - 1) * 30) + CInt(ComboBox2.SelectedItem.ToString)
+            End If
+        Else : MsgBox("Por favor selecciona un mes y una fecha")
+        End If
+        'Console.WriteLine(dia)
+    End Sub
+
+    Public Sub Evaluar()
+        For i = 1 To 26
+            If diaAño >= rangoA(i) And diaAño < rangoB(i) Then
+                grupo = i
+                cantidadMiembros = i + 1
+                Exit For
+            End If
+        Next
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Inicializar()
+        Evaluar()
+        MsgBox("Fecha: " & diaMes & "/" & mes & vbNewLine & "Grupo: " & grupo & vbNewLine & "Numero de integrantes: " & cantidadMiembros)
+    End Sub
+End Class
+
